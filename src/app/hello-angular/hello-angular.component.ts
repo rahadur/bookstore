2d092c17b86d11d7ca3dd8d1a2ac57dc462b684b
Rahadur Rahman <get.rahadur@gmail.com>
2022-02-19 13:03:29 +0600

    angular model and event

--- src/app/hello-angular/hello-angular.component.ts
+++ src/app/hello-angular/hello-angular.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello-angular',
   templateUrl: 'hello-angular.component.html',
 })
 export class HelloAngularComponent {
+
+  @Input() name: string = '';
+  
   constructor() {}
 }
