19d9d87e9fbfec835614657621c83762e325613d
Rahadur Rahman <get.rahadur@gmail.com>
2022-02-19 12:52:16 +0600

    Basic Component HelloAngular

---
+++ src/app/hello-angular/hello-angular.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-angular',
+  templateUrl: 'hello-angular.component.html',
+})
+export class HelloAngularComponent {
+  constructor() {}
+}
