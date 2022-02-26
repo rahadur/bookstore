da663dff430ea4958d48fcef6f5bac73491e8137
Rahadur Rahman <get.rahadur@gmail.com>
2022-02-26 11:04:36 +0600

    Directibes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  name: string = '';
+  
+  isTrue = false;
+
+  items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ]
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  showName(event: any): void {
-    console.log(event);
-    alert(this.name)
-  }
 }
