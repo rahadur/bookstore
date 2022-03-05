5b7e6510b2e96d2913d009f3655c97a2272ab3b7
Rahadur Rahman <get.rahadur@gmail.com>
2022-03-05 21:47:21 +0600

    Initial module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailsComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HelloAngularComponent } from './hello-angular/hello-angular.component';
-import { FormsModule } from '@angular/forms';
-import { ProductComponent } from './product/product.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HelloAngularComponent,
-    ProductComponent,
-    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
